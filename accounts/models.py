64549781b32aa1c8d8397094acdb1fb664e670cb
davidmikulis <davidmikulis@gmail.com>
2020-04-19 19:54:02 -0500

    Add database model for User
    Adds a database model with username, email, and password as fields

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    """Database model representing a user
+
+    Mandatory fields: username, email, password
+    """
+
+    username = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.username}({self.email})"
