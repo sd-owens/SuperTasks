2675c68339f54ed604acb8e1d40b3d829b330261
David Mikulis <davidmikulis@gmail.com>
2020-04-21 18:59:36 -0500

    Add registration view and form
    - Create a registration form based on the User model
    - Add a view to render a registration HTML for GET request
    - Add a view to add a User to the database for a POST request

---
+++ accounts/forms.py
@@ -0,0 +1,9 @@
+from django.forms import PasswordInput, modelform_factory
+from .models import User
+
+# Automatically creates a registration form from the User model
+RegisterForm = modelform_factory(
+    User,
+    fields=("username", "password", "email"),
+    widgets={"password": PasswordInput()}
+)
