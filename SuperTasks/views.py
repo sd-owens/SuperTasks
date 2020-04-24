5dee114aede503fa7744a74dc06618b9a36e4b06
David Mikulis <davidmikulis@gmail.com>
2020-04-24 17:57:15 -0500

    Fix #9
    Fixes issue with the home/index page not requiring a login

--- SuperTasks/views.py
+++ SuperTasks/views.py
@@ -2,13 +2,10 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def index(request):
     context = {}  # reserved for later use in rendering home
 
-    # Checks session cookie if they are logged in or not
-    if 'username' in request.session:
-        context['username'] = request.session['username']
-    
     return render(request, 'accounts/login.html', context)
