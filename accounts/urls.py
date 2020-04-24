ae5dc00165db6976cfb7702b4508a702b379badd
David Mikulis <davidmikulis@gmail.com>
2020-04-24 18:05:05 -0500

    Fix #9
    Adds a redirect to the login view that will not allow people who are
    already logged in to see the login page.

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,6 @@ from . import views # SuperTasks custom views
 urlpatterns = [
     path("test/", views.test_view, name="test"),
     path("register/", views.register_view, name="register"),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
