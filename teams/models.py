e557ae4c05813317a569263e2292aebb49137439
Steve Owens <sd.owens@outlook.com>
2020-05-15 08:56:47 -0700

    Created and registered Team Model and TeamForm, accessible through admin, established relational model for Teams in SuperTasks

--- teams/models.py
+++ teams/models.py
@@ -6,7 +6,8 @@ class Team(models.Model):
 
     team_name = models.CharField(max_length=50)
     users = models.ManyToManyField("accounts.Account")
-    tasks = models.ManyToManyField("projects.Feature")
+    task = models.ForeignKey("projects.Feature", on_delete=models.CASCADE, blank = True, null = True)
+    project = models.ForeignKey("projects.Project", on_delete=models.CASCADE, blank = True, null = True)
 
     def __str__(self):
         return self.team_name
