73f10435fb879fdd399a868c025833ea922a6a1a
David Mikulis <davidmikulis@gmail.com>
2020-05-15 17:59:32 -0500

    Adds view for new team form
    Right now this assumes that the Team model has a field called "accounts"
    for the ManyToMany with Account model. If that field is named "users"
    instead this view will need to be updated.

--- teams/views.py
+++ teams/views.py
@@ -3,20 +3,45 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db import Error
 
+from accounts.models import Account
 from .models import Team
 from .forms import TeamForm
 
-# Create your views here.
 
 @login_required
 def team_view(request):
-
-
     context = {}
     return render(request, "teams/team.html", context)
 
-#TODO new team 
-#@login_required
-#def new_team_view(request):
 
-   # return ren
+@login_required
+def new_team_view(request):
+    if request.method == 'GET':
+        accounts = Account.objects.all()
+
+        context = {'form': TeamForm(), 'accounts': accounts}
+        return render(request, "teams/new_team.html", context)
+
+    if request.method == 'POST':
+        form = TeamForm(request.POST)
+
+        if not form.is_valid():
+            context = {'form': form}
+
+            return render(request, 'teams/new_team.html', context)
+
+    name = request.POST['name']
+    accounts = request.POST.getlist('accounts')
+
+    try:
+        team = Team.objects.create(name=name)
+        for account_id in accounts:
+            team.accounts.add(account_id)
+        # Check where this redirects
+        return HttpResponseRedirect('/teams')
+    except Error as err:
+        context = {'error': str(err), 'form': form}
+        return render(request, 'teams/new_team.html', context)
+
+    # Return HTTP 405 Method Not Allowed
+    return HttpResponseNotAllowed(['POST', 'GET'])
