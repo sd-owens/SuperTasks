614ad734a816b596d4f76b52edc638c299e394cb
Steve Owens <sd.owens@outlook.com>
2020-05-17 08:38:53 -0700

    fixed error in forms.py for users vice accounts

--- teams/forms.py
+++ teams/forms.py
@@ -4,5 +4,5 @@ from .models import Team
 # Automatically creates a form from the model
 TeamForm = modelform_factory(
     Team,
-    fields=("name", "users", "projects"),  # a team can have a project or not upon creation
+    fields=("name", "accounts", "projects"),  # a team can have a project or not upon creation
 )
