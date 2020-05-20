d1562a377dfeac4dbadc8805b5563674c3264f1b
Steve Owens <sd.owens@outlook.com>
2020-05-20 12:15:15 -0700

    modified Teams model to allow creation with no associated accounts or projects

--- teams/models.py
+++ teams/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Team(models.Model):
 
     name = models.CharField(max_length=50)
-    accounts = models.ManyToManyField("accounts.Account")
+    accounts = models.ManyToManyField("accounts.Account", blank=True)
     projects = models.ManyToManyField("projects.Project", blank=True)
 
     def __str__(self):
