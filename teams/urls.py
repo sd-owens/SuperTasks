67ba726a2d4678ece358a2f6e19f8cfc30143fff
Steve Owens <sd.owens@outlook.com>
2020-05-21 09:36:39 -0700

    modified registered redirect to land at home page vice blank page, added basic navbar links for teams and projects until individual team and project views are added

--- teams/urls.py
+++ teams/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("", views.team_view, name="team"),
+    path("", views.team_view, name="team_view"),
 
     # Future Create new Teams Route.
     path("new/", views.new_team_view, name="team_new"),
