8ae28944111134e55ad6851d0ad795a4c2d16649
David Mikulis <davidmikulis@gmail.com>
2020-05-24 21:57:50 -0500

    Updates navbar with team view "name"
    Navbar uses reverse lookup on team view name to find the URL through the
    path, update template and URLs.

--- teams/urls.py
+++ teams/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.dashboard_view, name="dashboard"),
+    path("", views.dashboard_view, name="team_dashboard"),
 
 
     path("new/", views.new_team_view, name="new_team"),
