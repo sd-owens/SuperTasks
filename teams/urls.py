0ef5d82cad865e9bd08d4e3f884ed26cdac105eb
MESURF88 <hill.kevin@earthlink.net>
2020-05-24 22:26:27 -0400

    Update urls.py
    fix break?

--- teams/urls.py
+++ teams/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.dashboard_view, name="dashboard"),
-
+    #path("", views.dashboard_view, name="dashboard"),
+    path("", views.team_view, name="team_view"),
 
     path("new/", views.new_team_view, name="new_team"),
 
